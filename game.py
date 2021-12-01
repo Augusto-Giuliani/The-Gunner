8376f5b507193b1a28680dfbd3eee0f00068e598
NatanLewi <81169006+NatanLewi@users.noreply.github.com>
2021-12-01 10:15:05 -0300

    Implementando o sprite do avião (suporte aéreo)

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 # Importando as bibliotecas necessárias.
 import pygame as py
 import random as r
-from assets import load_assets, explosion_hit, explosion_miss, background,mission,shell_info,tank_info,support,ruler
-from sprites import Enemy_tank, Explosion, Explosion2, Ground, Howitzer,  Mine, Sergeant
-from values import EXPLODING, FPS, HEIGHT,PLAYING,QUIT,TRYAGAIN,VICTORY,RED,WIDTH,RULER_WIDTH,HOWITZER_WIDTH
+from assets import load_assets, explosion_hit, explosion_miss, background,mission, call, plane_info, plane_sound, shell_info,tank_info,support,ruler
+from sprites import Enemy_tank, Explosion, Explosion2, Ground, Howitzer,  Mine, Sergeant, Plane
+from values import BLACK,EXPLODING, FPS, HEIGHT,PLAYING,QUIT,TRYAGAIN,VICTORY,RED,WIDTH,RULER_WIDTH,HOWITZER_WIDTH,PLANE_INFO_WIDTH
 from screens import pause_screen
 
 # Criando a função com a estrutura fundamental do jogo.
@@ -16,10 +16,13 @@ def game_screen(screen,game_data):
     all_sprites = py.sprite.Group()
     all_enemies = py.sprite.Group()
     all_shells = py.sprite.Group()
+    all_rockets = py.sprite.Group()
+
     groups = dict()
     groups['all_sprites'] = all_sprites
     groups['all_shells'] = all_shells
     groups['all_enemies'] = all_enemies
+    groups['all_rockets'] = all_rockets
     # Criando o jogador (obuseiro e sargento).
     player = Howitzer(groups,assets)
     sergeant = Sergeant(assets)
@@ -38,11 +41,17 @@ def game_screen(screen,game_data):
     game_data['Shots taken'] = 0
     # Criando variável que contém o número de blindados/inimigos que precisam ser destruídos para cumprir a missão, dentro do dicionário com dados do jogo. 
     game_data['Mission'] = 20
+    # Criando variáveis que serão usadas em caso de suporte aéreo.
+    air_support = False # --> True quando o suporte aéreo for ativado.
+    activate_air_support = False # --> True quando o suporte aéreo pode ser ativado/está na "espera" (ícone do avião aparece no canto direito).
+    to_get_air_support = 2 # --> Quantos inimigos precisa destruír consecutivamente (sem erros) para conseguir suporte aéreo.
+    consecutive_hits_for_air_support = 0
     # Criando variável que vai ser usada para o suporte topográfico (dar o alcance médio no terreno, de cada carga, para o jogador).
     topografic_support = False
     activate_topographic_support = False
     to_get_topographic_support = game_data['Mission']/2 # --> Quando chegar na metade do objetivo, o suporte topográfico poderá ser acionado.
     # Variável para tocar o som de suporte uma vez.
+    support_sound_air = True
     support_sound_topo = True
     # Criando o inimigo.
     for i in range(3):
@@ -85,6 +94,17 @@ def game_screen(screen,game_data):
                         player.shoot(6,game_data)
                     elif event.key == py.K_7:
                         player.shoot(7,game_data)
+                    # Caso o suporte aéreo esteja "na espera" (ícone do avião apareceu na tela), a tecla "8" vai ativá-lo.
+                    elif activate_air_support and event.key == py.K_8:
+                        assets[call].play()
+                        assets[plane_sound].play()
+                        player_plane = Plane(groups,assets)
+                        all_sprites.add(player_plane)
+                        air_support = True # --> Ativando suporte aéreo.
+                        activate_air_support = False # --> Ícone some da tela.
+                    # Caso o suporte aéreo seja ativado, a tecla "8" é o gatilho para atirar o foguete.
+                    elif air_support and event.key == py.K_8:
+                        player_plane.shoot()
                     # Caso o suporte topográfico esteja "na espera" (ícone da régua apareceu na tela), a tecla "9" vai ativá-lo.
                     elif activate_topographic_support and event.key == py.K_9:
                         topografic_support = True # --> Ativando suporte topográfico.
@@ -99,7 +119,9 @@ def game_screen(screen,game_data):
         if STATE == PLAYING:
             # Verificando se houve colisão entre o projétil e o inimigo, entre o projétil e o "chão", entre o inimigo e o obuseiro, entre o foguete e o "chão", entre o foguete e o inimigo e entre a mina e o inimigo.
             hits = py.sprite.groupcollide(all_enemies, all_shells, True, True, py.sprite.collide_mask)
+            hits2 = py.sprite.groupcollide(all_enemies, all_rockets, True, True, py.sprite.collide_mask)
             misses = py.sprite.spritecollide(floor, all_shells, True, py.sprite.collide_mask)
+            misses2 = py.sprite.spritecollide(floor, all_rockets, True, py.sprite.collide_mask)
             collision = py.sprite.spritecollide(player, all_enemies, True, py.sprite.collide_mask)
             mine_hit = py.sprite.spritecollide(mine, all_enemies, True, py.sprite.collide_mask)
 
@@ -130,6 +152,17 @@ def game_screen(screen,game_data):
                 explosion = Explosion2(enemy.rect.centerx,enemy.rect.centery - 55,assets)
                 all_sprites.add(explosion)
                 enemy_down +=1
+            for enemy in hits2:
+                assets[explosion_hit].play()
+                if enemy_created < game_data['Mission']: # --> Não aparecer inimigos a mais do que a missão já estebelece.
+                    e = Enemy_tank(assets,r.choice([1,2,3]))
+                    all_sprites.add(e)
+                    all_enemies.add(e)
+                    enemy_created += 1
+                # No lugar do inimigo destruído, adicionando uma explosão.
+                explosion = Explosion2(enemy.rect.centerx,enemy.rect.centery - 55,assets)
+                all_sprites.add(explosion)
+                enemy_down +=1
             # Caso o projétil atinja o "chão".
             for shell in misses:
                 consecutive_hits_for_air_support = 0 
@@ -137,6 +170,10 @@ def game_screen(screen,game_data):
                 assets[explosion_miss].play()
                 explosion = Explosion(shell.rect.centerx,shell.rect.centery - 30,assets)
                 all_sprites.add(explosion)  
+            for rocket in misses2:
+                assets[explosion_miss].play()
+                explosion = Explosion(rocket.rect.centerx,rocket.rect.centery - 30,assets)
+                all_sprites.add(explosion)
             # Caso o inimigo atinja o obuseiro (player), GAME OVER.
             for enemy in collision:
                 assets[explosion_hit].play()
@@ -155,6 +192,14 @@ def game_screen(screen,game_data):
         # Caso um número (mission) de blindados/inimigos seja destruído, vitória do usuário.
         if enemy_down >= game_data['Mission']:
             STATE = VICTORY
+        # Caso o número de inimigos destruídos consecutivamente seja atinjido, o suporte aéreo poderá ser ativado (modo de "espera"; ícone aparece).
+        if consecutive_hits_for_air_support == to_get_air_support and not air_support and not activate_air_support:
+            activate_air_support = True
+            support_sound_air = True
+            consecutive_hits_for_air_support = 0
+        # Caso o avião já tenha saído da tela...
+        if air_support and player_plane.rect.x > WIDTH:
+            air_support = False
         # Caso o número de inimigos destruídos atinja o previsto para suporte topográfico.
         if enemy_down >= to_get_topographic_support and not topografic_support:
             activate_topographic_support = True 
@@ -184,6 +229,13 @@ def game_screen(screen,game_data):
         text_rect.bottomleft = (WIDTH - 110,130)
         screen.blit(text_shots,text_rect) # --> Dado numérico.
         screen.blit(assets[shell_info],(WIDTH - 240,20)) # --> Ícone.
+        # Desenhando o ícone do avião caso o suporte aéreo possa ser ativado.
+        if activate_air_support:
+            consecutive_hits_for_air_support = 0
+            if support_sound_air:
+                assets[support].play()
+                support_sound_air = False
+            screen.blit(assets[plane_info],(WIDTH - PLANE_INFO_WIDTH - 10,300)) # --> Ícone.
         # Desenhando o ícone da régua caso o suporte topográfico possa ser ativado.
         if activate_topographic_support:
             if support_sound_topo:
